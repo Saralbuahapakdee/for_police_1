0ab02fa3e21c40e15aa473b33e605788bc382a33
Saralbuahapakdee <110214150+Saralbuahapakdee@users.noreply.github.com>
2026-03-30 23:15:32 +0700

    Delete notworkingone directory

--- notworkingone/backend/fix_db.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-c=sqlite3.connect('users.db')
-c.execute("UPDATE cameras SET mqtt_topic='cam1/' WHERE id=1")
-c.execute("UPDATE cameras SET mqtt_topic='cam2/' WHERE id=2")
-c.commit()
-print("Updated database")
